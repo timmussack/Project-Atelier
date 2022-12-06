72afd4c4166d206ab6047a831c94a4abafa34240
Darrien <darrienpatriot@gmail.com>
2022-12-06 13:57:15 -0500

    Added navbar

---
+++ Client/src/NavBar.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import styled from 'styled-components'
+
+
+const HeaderContainer = styled.div`
+  display: flex;
+  position: relative;
+  justify-content: space-between;
+  align-items: center;
+  background-color: grey;
+  margin-right: 150px;
+  margin-left: 150px;
+  height: 80px;
+
+`
+
+
+const SearchBarContainer = styled.div`
+  position: relative;
+  width: 20%;
+  padding-right: 50px
+`
+
+const SearchBar = styled.input`
+  position: relative;
+  width: 75%;
+  height: 2.8rem;
+  background: #f5f5f5;
+  outline: none;
+  border: none;
+  border-radius: 1.625rem;
+  padding: 0 3.5rem 0 1.5rem;
+  font-size: 1rem;
+`
+const Logo = styled.h1`
+  font-size: 2em;
+  padding-top: 20px;
+  margin: 0 0 15px 30px;
+`
+
+const NavBar =  () => {
+  return (
+    <HeaderContainer>
+        <Logo>LogoHere</Logo>
+        <SearchBarContainer>
+          <SearchBar type="text" name="searchQueryInput" placeholder="Search" value=""/>
+        </SearchBarContainer>
+    </HeaderContainer>
+
+  )
+}
+
+
+export default NavBar;
