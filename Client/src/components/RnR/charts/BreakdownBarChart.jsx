50d3faece44cefbd4bbbaa4d933c2427294ce93c
Joe Lin <joelin3478@gmail.com>
2022-12-15 15:43:41 -0500

    Update BreakdownBarChart.jsx

--- Client/src/components/RnR/charts/BreakdownBarChart.jsx
+++ Client/src/components/RnR/charts/BreakdownBarChart.jsx
@@ -3,14 +3,6 @@ import Stars from '../Stars.jsx';
 
 const BreakdownBarChart = ({ metaData }) => {
 
-  const displayChars = (characteristics) => {
-    let chars = ['Too small', 'Perfect', 'Too big']
-    if (characteristics === 'Quality') {
-      chars = ['Poor', 'Perfect']
-    }
-    return chars.map((char) => <span key={char}>{char}</span>)
-  }
-
   return (
     <>
     { Object.keys(metaData.characteristics).map((characteristic, index) => {
