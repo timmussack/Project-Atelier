a2e86af13bca2ed084da64adca77022a798ce0b3
Tim Mussack <tim.mussack@gmail.com>
2022-12-14 21:17:39 -0600

    Started night mode implementation, improved button css, centered photos when they are zoomed

---
+++ Client/src/NightMode.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import styled, { css } from 'styled-components';
+import '../dist/styles.css';
+
+const NightModeContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`;
+
+const NightModeText = styled.a`
+	position: relative;
+	color: white;
+`;
+
+const NightModeButtonWrapper = styled.div`
+	align-self: center;
+	margin: 0px 10px 20px 10px;
+	position: relative;
+`;
+
+const NightMode = ({ nightMode, setNightMode }) => {
+
+	return (
+    <NightModeContainer>
+			<NightModeText>Night Mode</NightModeText>
+			<NightModeButtonWrapper>
+				<input
+					className="react-switch-checkbox"
+					id={`react-switch-new`}
+					type="checkbox"
+					onChange={() => setNightMode(!nightMode)}
+				/>
+				<label
+					className="react-switch-label"
+					htmlFor={`react-switch-new`}
+				>
+					<span className={`react-switch-button`} />
+				</label>
+			</NightModeButtonWrapper>
+
+    </NightModeContainer>
+  );
+};
+
+export default NightMode;
